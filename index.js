e0364f149424a412a83aecf5f25076f5e9f3934c
sidee <said.issack@student.moringaschool.com>
2023-06-24 09:54:21 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+const mainNode = document.getElementById("main");
+mainNode.remove();
+document.body.appendChild(newHeader);
